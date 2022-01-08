0fdc9989c2d4327a491a2bf723b04b0105c08d2b
FarDean <64462409+FarDean@users.noreply.github.com>
2022-01-08 16:26:15 +0330

    added some stuff

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
-import { Buffer } from "buffer";
+import { Buffer } from 'buffer';
 
-function bufferName(name: string) {
-	const buffer = Buffer.from(name);
-	return buffer.toString("hex");
+export function bufferName(name: string) {
+  const buffer = Buffer.from(name);
+  return buffer.toString('hex');
 }
+console.log(bufferName('fardean'));
